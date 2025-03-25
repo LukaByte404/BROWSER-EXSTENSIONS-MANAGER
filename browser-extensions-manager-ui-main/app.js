2965f51af8afe485cfa77e3230eab7024468f7a7
ChikhladzeLuka <lukachikhladze587@gmail.com>
2025-03-25 12:52:46 +0400

    add data from data.json to html and a little style for these data

--- browser-extensions-manager-ui-main/app.js
+++ browser-extensions-manager-ui-main/app.js
@@ -1,5 +1,28 @@
-fetch("data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const result = document.querySelector(".result-section");
-  });
+const resultSec = document.querySelector(".result-section");
+
+fetch("../browser-extensions-manager-ui-main/data.json").then((response) =>
+  response
+    .json()
+    .then((data) => {
+      data.forEach((item) => {
+        const card = document.createElement("div");
+        card.classList.add(".card");
+
+        const resImage = document.createElement("img");
+        resImage.src = item.logo;
+
+        const resTilte = document.createElement("h3");
+        resTilte.innerHTML = item.name;
+
+        const resDesc = document.createElement("p");
+        resDesc.innerHTML = item.description;
+
+        card.appendChild(resImage);
+        card.appendChild(resTilte);
+        card.appendChild(resDesc);
+
+        resultSec.appendChild(card);
+      });
+    })
+    .catch((error) => console.log(error))
+);
